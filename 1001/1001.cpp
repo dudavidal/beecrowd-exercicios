bb9ba39ed9ad29aab1032a9824ad112ef76e9067
Maria Eduarda Vidal <164089944+dudavidal@users.noreply.github.com>
2025-08-11 14:31:11 -0300

    Create 1001.cpp

---
+++ 1001/1001.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    int a;
+    int b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int x = a + b;
+    printf("X = %d\n", x);  // Note o \n aqui para pular linha
+
+    return 0;
+}
