b099503f14a4c0365034a5591ddfe99932f778a3
Maria Eduarda Vidal <164089944+dudavidal@users.noreply.github.com>
2025-08-12 12:00:27 -0300

    Create 1008.py

---
+++ 1008/1008.py
@@ -0,0 +1,9 @@
+numeroFuncioanrio = int(input())
+numeroHoras = int(input())
+qtdRecebe = float(input())
+
+
+salario = numeroHoras * qtdRecebe
+
+print(f'NUMBER = {numeroFuncioanrio}')
+print(f'SALARY = U$ {salario:.2f}')
