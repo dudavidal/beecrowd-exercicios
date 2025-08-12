cbce35315c78a3f520878ec1fab573d41b65a1b0
Maria Eduarda Vidal <164089944+dudavidal@users.noreply.github.com>
2025-08-12 11:10:43 -0300

    Create 1002.c

---
+++ 1002/1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    double pi = 3.14159;
+    double raio;
+    double area;
+    
+    scanf("%lf",&raio);
+    
+    area = pi* (raio *raio);
+    printf("A=%.4lf\n",area);
+    
+    
+
+    return 0;
+}
